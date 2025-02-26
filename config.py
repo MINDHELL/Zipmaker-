230ec51c1a233baee9ca99e8889de43d81dc02f5
MINDHELL <Tgpurpose640@gmail.com>
2025-02-26 20:34:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "27788368"))  # Replace with your API ID
+API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")  # Replace with your API HASH
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8064879322:AAH4Uv8ZJbHfDZRBnre_Uf4D-ew-Q8SCinc")  # Replace with your bot token
