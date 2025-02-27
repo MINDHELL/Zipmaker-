e78d9f47061ff5f91857080a965918f3b1092677
MINDHELL <Tgpurpose640@gmail.com>
2025-02-27 20:03:59 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ def health_check():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8000)  # Running on Port 8000 for Koyeb
